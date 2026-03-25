831e1244a1d84d3e4b6ce35c3c2313feae65a255
pengyu <pengyuchen01@gmail.com>
2026-03-24 22:30:56 -0400

    adding cli lib and buffer lib

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,12 +1,28 @@
-
+use clap::Parser;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+// search for a pattern in a file and display the lines that contain it
+#[derive(Parser)]
 struct Cli{
     pattern: String,
     path: std::path::PathBuf,
 }
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
+    // let pattern = std::env::args().nth(1).expect("no pattern given");
+    // let path = std::env::args().nth(2).expect("no path given");
+    // let arg = Cli {
+    //     pattern,
+    //     path: std::path::PathBuf::from(path),
+    // };
+    let args = Cli::parse();
+    let file = File::open(&args.path).expect("could not open file");
+    let reader = BufReader::new(file);
 
-    println!("pattern: {:?}, path: {:?}", pattern, path)
+    for line in reader.lines() {
+        let line = line.expect("could not read line");
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
--- grrs/target/debug/deps/grrs-4aab353fe065f159.d
+++
@@ -1,5 +0,0 @@
-/home/chen/rust_cli/grrs/target/debug/deps/grrs-4aab353fe065f159: src/main.rs
-
-/home/chen/rust_cli/grrs/target/debug/deps/grrs-4aab353fe065f159.d: src/main.rs
-
-src/main.rs:
